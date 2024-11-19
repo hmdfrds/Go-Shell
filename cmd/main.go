7d5035aa682d3971b9d64e5e3e6f051d4df42301
firdaus <firdaus1337@gmail.com>
2024-11-19 23:45:19 +0800

    now can read input

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	CD = "cd"
+)
 
 func main() {
-	fmt.Println("Miaw")
+
+	fmt.Print("Your Input: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	cmd, _ := extractCmdArgs(input)
+
+	fmt.Println("Your command is ", cmd)
+}
+
+func extractCmdArgs(input string) (string, []string) {
+	if isEmptyString(input) {
+		return "", nil
+	}
+
+	cmd := strings.Split(input, " ")[0]
+
+	if !contains(getAvailableCommand(), cmd) {
+		return "", nil
+	}
+
+	return cmd, nil
+}
+
+func contains(slice []string, cmd string) bool {
+	for _, availableCmd := range slice {
+		if availableCmd == cmd {
+			return true
+		}
+	}
+	return false
+}
+
+func isEmptyString(str string) bool {
+	return len(strings.TrimSpace(str)) == 0
+}
+
+func getAvailableCommand() []string {
+	return []string{CD}
 }
